80c0cafaf9484c2dba2926b6ce133e69865a2f5c
Adam Averay <adam@averay.com>
2023-11-17 11:12:37 +0100

    Improve typehinting

--- packages/sfconnect/src/Slices/SearchInterface.php
+++ packages/sfconnect/src/Slices/SearchInterface.php
@@ -3,16 +3,20 @@ declare(strict_types=1);
 
 namespace Averay\SfConnect\Slices;
 
+use Averay\SfConnect\Objects\Record;
+
 /** @psalm-require-extends \Averay\SfConnect\ClientInterface */
 interface SearchInterface
 {
   /**
    * @see https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_search.htm
+   * @return list<Record>
    */
   public function search(string|\Averay\SfConnect\Querying\SearchInterface $query): array;
 
   /**
    * @see https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_search_parameterized_get.htm
+   * @return list<Record>
    */
   public function parameterizedSearch(string $string, array $params = []): array;
 }
